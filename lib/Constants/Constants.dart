1b34eba6f2fb58e54b64b736cd3d12122cd386bf
Mohamed Moughamir <moughamir@gmail.com>
2021-02-28 15:32:06 +0100

    Platform error messages constants

---
+++ lib/Constants/Constants.dart
@@ -0,0 +1,15 @@
+const ANDROID_ERROR_USER_NOT_FOUND =
+    'There is no user record corresponding to this identifier. The user may have been deleted.';
+
+const ANDROID_ERROR_INVALIDE_USERNAME_PASSWORD =
+    'The password is invalid or the user does not have a password.';
+
+const ANDROID_ERROR_USER_ALREADY_EXIST =
+    'The email address is already in use by another account.';
+
+const ANDROID_ERROR_TIME_OUT =
+    'A network error (such as timeout, interrupted connection or unreachable host) has occurred.';
+
+const IOS_ERROR_17011 = 'Error 17011';
+const IOS_ERROR_17009 = 'Error 17009';
+const IOS_ERROR_17020 = 'Error 17020';
